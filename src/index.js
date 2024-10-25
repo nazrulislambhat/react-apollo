0f9fa498c66de4b68e59b961043f6aa9459bd779
Nazrul Islam <nazrul.islam@ama-assn.org>
2024-10-25 12:16:29 +0530

    +

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client'; // Updated import
 import './styles/index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Set up Apollo Client
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+// Get the root element
+const rootElement = document.getElementById('root');
+
+// Render the App with ApolloProvider
+if (rootElement) {
+  const root = createRoot(rootElement); // Create a root
+  root.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+}
